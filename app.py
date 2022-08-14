19d7d6d40464251b15df4781d89c8a4035bafb89
hapi2 <rock666gemini69@gmail.com>
2022-08-14 11:22:57 +0900

    一つ前が壊れたので作り直しました。 venv使ってるからなのか、コミット内容が2680個にもなった。

---
+++ app.py
@@ -0,0 +1,54 @@
+from flask import Flask
+from flask import render_template, request, redirect
+import psycopg2
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import pytz
+ 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://localhost/fblog"
+db = SQLAlchemy(app)
+ 
+ 
+class BlogArticle(db.Model):
+    __tablename__ = "article"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), nullable=False)
+    body = db.Column(db.String(500), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now(pytz.timezone('Asia/Tokyo')))
+ 
+@app.route('/', methods=['GET'])
+def blog():
+    if request.method == 'GET':
+        blogarticles = BlogArticle.query.all()
+        return render_template('index.html', blogarticles=blogarticles)
+
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+    if request.method == "POST":
+        title = request.form.get('title')
+        body = request.form.get('body')
+        blogarticle = BlogArticle(title=title, body=body)
+        db.session.add(blogarticle)
+        db.session.commit()
+        return redirect('/')
+    else:
+        return render_template('create.html')
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    blogarticle = BlogArticle.query.get(id)
+    if request.method == "GET":
+        return render_template('update.html', blogarticle=blogarticle)
+    else:
+        blogarticle.title = request.form.get('title')
+        blogarticle.body = request.form.get('body')
+        db.session.commit()
+        return redirect('/')
+
+@app.route('/delete/<int:id>', methods=['GET'])
+def delete(id):
+    blogarticle = BlogArticle.query.get(id)
+    db.session.delete(blogarticle)
+    db.session.commit()
+    return redirect('/')
