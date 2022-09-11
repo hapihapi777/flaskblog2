dffa72b8bcdf24050b3127f5794a20d149a93dd6
hapi2 <rock666gemini69@gmail.com>
2022-09-11 12:58:09 +0900

    .gitignore update

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from curses import flash
+# from curses import flash
 import os
-from flask import Flask
-from flask import render_template, request, redirect
+import re
+from flask import Flask, render_template, request, redirect, url_for
 from flask_login import UserMixin, LoginManager, login_user, logout_user, login_required
 import psycopg2
 from flask_sqlalchemy import SQLAlchemy
@@ -11,13 +11,10 @@ from werkzeug.security import generate_password_hash, check_password_hash
  
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(24)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://localhost/fblog"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sqliteblog.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://localhost/fblog"
 # app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://hcrrnqrjoezdpt:561263e6bcbfc2d99e39c56c0d67816eecedfcc6362cd0d7daaa7523d3166fad@ec2-3-225-110-188.compute-1.amazonaws.com:5432/d78h3uhegfieod"
 db = SQLAlchemy(app)
-
-# デバッグモード用
-if __name__ == "__main__":
-    app.run(debug=True)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -26,11 +23,12 @@ login_manager.init_app(app)
 class BlogArticle(db.Model):
     __tablename__ = "article"
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(), nullable=False)
-    body = db.Column(db.String(), nullable=False)
+    title = db.Column(db.String(30), nullable=False)
+    body = db.Column(db.String(20), nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.now(pytz.timezone('Asia/Tokyo')))
   
 class User(UserMixin, db.Model):
+    __tablename__ = "signup"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(10))
@@ -40,14 +38,14 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 # topページ
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def blog():
     # if request.method == 'GET':
         blogarticles = BlogArticle.query.all()
         return render_template('index.html', blogarticles=blogarticles)
 
 # 編集可能なmasterページにPOSTで来た場合(正常動作)とそれ以外に設定
-@app.route('/master', methods=['POST'])
+@app.route('/master', methods=['GET', 'POST'])
 @login_required
 def master():
     if request.method == "POST":
@@ -56,11 +54,6 @@ def master():
         return render_template('/master.html', blogarticles=blogarticles)
     else:
         return redirect('/')
-
-# errorページに飛ぶ用
-@app.route('/error', methods=['GET', 'POST'])
-def error():
-    return render_template('/error.html')
 
 # signupページに飛ぶだけ
 @app.route('/signup', methods=['GET', 'POST'])
@@ -89,15 +82,16 @@ def do_signup():
         
         db.session.add(user)
         db.session.commit()
-        flash('登録に成功')
-        return render_template('/login')
+        # flash('登録に成功')
+        return redirect('/login')
     else:
-        flash('登録に失敗')
+        # flash('登録に失敗')
         return render_template('/error.html')
 
 # 登録済みのuserが入力された場合にのみmasterページに飛ぶ関数
 @app.route('/do_login', methods=['GET', 'POST'])
 def do_login():
+    blogarticles = BlogArticle.query.all()
     if request.method == "POST":
         username = request.form.get('username')
         password = request.form.get('password')
@@ -105,19 +99,12 @@ def do_login():
         user = User.query.filter_by(username=username).first()
         if check_password_hash(user.password, password):
             login_user(user)
-            return redirect('/master.html')
+            return render_template('/master.html', blogarticles=blogarticles)
         else:
-            flash('入力に失敗')
-            render_template('/error.html')
+            # flash('入力に失敗')
+            return render_template('/login.html')
     else:
-        return render_template('/error.html')
-
-# ログアウト用
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect('/')
+        return render_template('/login.html')
 
 # 新規作成画面
 @app.route('/create', methods=['GET', 'POST'])
@@ -126,7 +113,7 @@ def craete():
     if request.method == "POST":
         return render_template('create.html')
     else:
-        return redirect('/')
+        return redirect(url_for('master'))
 
 # 新規作成メソッド
 @app.route('/do_create', methods=['GET', 'POST'])
@@ -138,9 +125,10 @@ def do_create():
         blogarticle = BlogArticle(title=title, body=body)
         db.session.add(blogarticle)
         db.session.commit()
-        return redirect('/')
+        blogarticles = BlogArticle.query.all()
+        return render_template('/master.html', blogarticles=blogarticles)
     else:
-        return redirect('/')
+        return redirect(url_for('master'))
 
 # updateにPOSTで来た場合(正常動作)
 # とそれ以外に設定
@@ -152,7 +140,7 @@ def update():
         blogarticle = BlogArticle.query.filter(BlogArticle.id == post_id).one()
         return render_template('update.html', blogarticle=blogarticle)
     else:
-        return redirect('/')
+        return redirect(url_for('master'))
 
 # updateページから更新する場合
 @app.route('/do_update', methods=['POST'])
@@ -164,7 +152,8 @@ def do_update():
     blogarticle.body = request.form.get('body')
     db.session.add(blogarticle)
     db.session.commit()
-    return redirect('/')
+    blogarticles = BlogArticle.query.all()
+    return render_template('/master.html', blogarticles=blogarticles)
 
 # 削除する場合(現状一発で削除されてしまう)
 @app.route('/delete', methods=['POST'])
@@ -174,8 +163,11 @@ def delete():
     blogarticle = BlogArticle.query.filter(BlogArticle.id == post_id).one()
     db.session.delete(blogarticle)
     db.session.commit()
+    return redirect(url_for('master'))
+
+# ログアウト用
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
     return redirect('/')
-
-
-
-# 9/1 テスト
