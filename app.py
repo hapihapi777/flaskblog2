842dc8005cfd8df0c97a40ec1b8e5344d2690d6e
hapi2 <rock666gemini69@gmail.com>
2022-10-04 23:46:41 +0900

    cookieの設定、削除をしました

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # from curses import flash
 import os, psycopg2, pytz
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, make_response, session, redirect, url_for
 from flask_login import UserMixin, LoginManager, login_user, logout_user, login_required
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
@@ -10,8 +10,6 @@ app = Flask(__name__)
 # app.config['SECRET_KEY'] = os.urandom(24)
 app.config['SECRET_KEY'] = "secret"
 
-# DATABASE_URL = os.environ['DATABASE_URL']
-# conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sqliteblog.db'
 # app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://localhost/fblog2"
@@ -35,11 +33,13 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(100))
 
-Display = {'username': '', 'エラーコメント': ''}
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
+
+
 
 # topページ
 @app.route('/', methods=['GET', 'POST'])
@@ -47,22 +47,15 @@ def blog():
     blogarticles = BlogArticle.query.all()
     return render_template('index.html', blogarticles=blogarticles)
 
-
 # signupページに飛ぶだけ
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    # if request.method == "POST":
         return render_template('/signup.html')
-    # else:
-    #     return redirect(url_for('logout'))
 
 # loginページに飛ぶだけ
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    # if request.method == "POST":
         return render_template('/login.html')
-    # else:
-    #     return redirect(url_for('logout'))
 
 # signupページ内でuser登録をする為の関数
 @app.route('/do_signup', methods=['GET', 'POST'])
@@ -72,11 +65,9 @@ def do_signup():
         password = request.form.get('register_pass')
         # Userのインスタンスを作成
         user = User(username=username, password=generate_password_hash(password, method='sha256'))
-        
         db.session.add(user)
         db.session.commit()
         # flash('登録に成功')
-        # return render_template('login.html')
         return redirect(url_for('login'))
     else:
         # flash('登録に失敗')
@@ -94,57 +85,49 @@ def do_login():
 
         if check_password_hash(user.password, password):
             login_user(user)
-            # blogarticles = BlogArticle.query.all()
-            # Display.l_user = username
-            Display['username'] = username
-            # return render_template('/master.html', blogarticles=blogarticles, username=username)
-            return redirect(url_for('master'))
+            res = make_response(redirect(url_for('master')))
+            res.set_cookie('l_username', username)
+            return res
         else:
             # flash('入力に失敗')
             return render_template('/login.html')
     else:
         return redirect(url_for('logout'))
 
+
+
+
+
 # 編集可能なmasterページにPOSTで来た場合(正常動作)とそれ以外に設定
 @app.route('/master', methods=['GET', 'POST'])
 @login_required
 def master():
-    # if request.method == "POST":
-        # username = Display.l_user
-        username = Display['username']
-        blogarticles = BlogArticle.query.all()
-        return render_template('/master.html', blogarticles=blogarticles, username=username)
-    # else:
-    #     return redirect(url_for('logout'))
+    l_username = request.cookies.get('l_username')
+    blogarticles = BlogArticle.query.all()
+    return render_template('/master.html', blogarticles=blogarticles, username=l_username)
 
 # 新規作成画面
 @app.route('/create', methods=['GET', 'POST'])
 @login_required
 def create():
-    # if request.method == "POST":
-        # comment = Display['エラーコメント']
-        # Display['エラーコメント'] = ''
-        return render_template('create.html', username=Display['username'], comment=Display['エラーコメント'])
-    # else:
-    #     return redirect(url_for('logout'))
+    l_username = request.cookies.get('l_username')
+    return render_template('create.html', 
+    username=l_username
+    )
 
 # 新規作成メソッド
 @app.route('/do_create', methods=['GET', 'POST'])
 @login_required
 def do_create():
     if request.method == "POST":
-        # username = Display['username']
         title = request.form.get('title')
         body = request.form.get('body')
         if title == "":
-            Display['エラーコメント'] = '＊タイトルを入れてください'
             return redirect(url_for('create'))
         else:
             blogarticle = BlogArticle(title=title, body=body)
             db.session.add(blogarticle)
             db.session.commit()
-            Display['エラーコメント'] = ''
-            # blogarticles = BlogArticle.query.all()
             return redirect(url_for('master'))
     else:
         return redirect(url_for('logout'))
@@ -154,13 +137,10 @@ def do_create():
 @app.route('/update', methods=['GET', 'POST'])
 @login_required
 def update():
-    # if request.method == "POST":
-        post_id = request.form.get("post_id")
-        blogarticle = BlogArticle.query.filter(BlogArticle.id == post_id).one()
-        username = request.form.get('username')
-        return render_template('update.html', blogarticle=blogarticle, username=username)
-    # else:
-    #     return redirect(url_for('logout'))
+    post_id = request.form.get("post_id")
+    blogarticle = BlogArticle.query.filter(BlogArticle.id == post_id).one()
+    l_username = request.cookies.get('l_username')
+    return render_template('update.html', blogarticle=blogarticle, username=l_username)
 
 # updateページから更新する場合
 @app.route('/do_update', methods=['GET', 'POST'])
@@ -177,8 +157,6 @@ def do_update():
             blogarticle.body = request.form.get('body')
             db.session.add(blogarticle)
             db.session.commit()
-            # blogarticles = BlogArticle.query.all()
-            # return render_template('/master.html', blogarticles=blogarticles, username=username)
             return redirect(url_for('master'))
     else:
         return redirect(url_for('logout'))
@@ -192,9 +170,6 @@ def delete():
         blogarticle = BlogArticle.query.filter(BlogArticle.id == post_id).one()
         db.session.delete(blogarticle)
         db.session.commit()
-        # blogarticles = BlogArticle.query.all()
-        # username = request.form.get('username')
-        # return render_template('/master.html', blogarticles=blogarticles, username=username)
         return redirect(url_for('master'))
     else:
         return redirect(url_for('logout'))
@@ -204,6 +179,6 @@ def delete():
 @login_required
 def logout():
     logout_user()
-    Display['username'] = ''
-    Display['エラーコメント'] = ''
-    return redirect('/')
+    res = make_response(redirect(url_for('blog')))
+    res.delete_cookie('l_username')
+    return res
