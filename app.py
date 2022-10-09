476e677d0d1d9194471778ba514ef7d0581b75ca
hapi2 <rock666gemini69@gmail.com>
2022-10-09 12:47:14 +0900

    do_updateの修正

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from datetime import timedelta
  
 app = Flask(__name__)
 # app.config['SECRET_KEY'] = os.urandom(24)
-app.config['SECRET_KEY'] = "secret"
+app.config['SECRET_KEY'] = "abcdefghijklmn"
 app.permanent_session_lifetime = timedelta(minutes=3)
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sqliteblog.db'
@@ -56,12 +56,12 @@ def blog():
 # signupページに飛ぶだけ
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-        return render_template('/signup.html')
+    return render_template('/signup.html')
 
 # loginページに飛ぶだけ
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-        return render_template('/login.html')
+    return render_template('/login.html')
 
 # signupページ内でuser登録をする為の関数
 @app.route('/do_signup', methods=['GET', 'POST'])
@@ -69,26 +69,25 @@ def do_signup():
     if request.method == "POST":
         username = request.form.get('register_user')
         password = request.form.get('register_pass')
-        # Userのインスタンスを作成
-        if username == "":
-            flash('登録に失敗')
-            return redirect(url_for('signup'))
-        elif password == "":
-            flash('登録に失敗')
-            return redirect(url_for('signup'))
-        else:
+        if username != "" and password != "":
+            # Userのインスタンスを作成
             user = User(username=username, password=generate_password_hash(password, method='sha256'))
             db.session.add(user)
             db.session.commit()
-            # flash('登録に成功')
+            flash('登録に成功')
             return redirect(url_for('login'))
-    else:
-        flash('登録に失敗')
+        if username == "":
+            flash('usernameが空欄です')
+        if password == "":
+            flash('passwordが空欄です')
         return redirect(url_for('signup'))
-
-
+    else:
+     flash('登録に失敗')
+     return redirect(url_for('signup'))
 
 # 登録済みのuserが入力された場合にのみmasterページに飛ぶ関数
+# ログインに必要なデータ入力もあるのでPOSTに限定
+# cookieとsessionを設定する
 @app.route('/do_login', methods=['GET', 'POST'])
 def do_login():
     if request.method == "POST":
@@ -97,12 +96,9 @@ def do_login():
 
         # Userテーブルからusernameに一致するユーザを取得
         user = User.query.filter_by(username=username).first()
-
-
         if check_password_hash(user.password, password):
             login_user(user)
-            session.permanent = True  
-            # l_user = request.form.get('username')
+            session.permanent = True
             session["l_username"] = username
             res = make_response(redirect(url_for('master')))
             res.set_cookie('l_username', username)
@@ -117,7 +113,8 @@ def do_login():
 
 
 # ---ログイン後ページ---
-# 編集可能なmasterページにPOSTで来た場合(正常動作)とそれ以外に設定
+# 編集可能なmasterページ
+# ログイン中ならGET,POST共に接続可能にする
 @app.route('/master', methods=['GET', 'POST'])
 @login_required
 def master():
@@ -125,7 +122,9 @@ def master():
     blogarticles = BlogArticle.query.all()
     return render_template('/master.html', blogarticles=blogarticles, username=l_username)
 
+
 # 新規作成画面
+# 飛ぶだけなのでログイン中ならGET,POST共に接続可能にする
 @app.route('/create', methods=['GET', 'POST'])
 @login_required
 def create():
@@ -135,6 +134,7 @@ def create():
     )
 
 # 新規作成メソッド
+# データを扱う作業なので、接続中でもPOSTメソッドのみ
 @app.route('/do_create', methods=['GET', 'POST'])
 @login_required
 def do_create():
@@ -154,56 +154,53 @@ def do_create():
     else:
         return redirect(url_for('logout'))
 
-# updateにPOSTで来た場合
-# とそれ以外に設定
+# updateに飛ぶだけ
+# GET,POST共に接続可能
 @app.route('/update', methods=['GET', 'POST'])
 @login_required
 def update():
     post_id = request.form.get("post_id")
-    blogarticle = BlogArticle.query.filter(BlogArticle.id == post_id).one()
+    blogarticle = BlogArticle.query.filter(BlogArticle.id == post_id).first()
     l_username = request.cookies.get('l_username')
     res = make_response(render_template('update.html', blogarticle=blogarticle, username=l_username))
-    res.set_cookie("post_id", post_id)
+    # res.set_cookie("post_id", post_id)
     return res
 
 # updateページから更新する場合
+# データ扱うのでPOSTのみ
 @app.route('/do_update', methods=['GET', 'POST'])
 @login_required
 def do_update():
     if request.method == "POST":
         post_id = request.form.get("post_id")
         
-        blogarticle = BlogArticle.query.filter(BlogArticle.id == post_id).one()
+        blogarticle = BlogArticle.query.filter(BlogArticle.id == post_id).first()
         title = request.form.get('title')
         body = request.form.get('body')
         if title != "" and body != "":
-            blogarticle = BlogArticle(title=title, body=body)
+            blogarticle.title = title
+            blogarticle.body = body
             db.session.add(blogarticle)
             db.session.commit()
+
             return redirect(url_for('master'))
         if title == "":
             flash("タイトルが空欄です")
         if body == "":
             flash("記事が空欄です")
         return redirect(url_for('update'))
-        # if request.form.get('title') == "":
-        #     return render_template('update.html', blogarticle=blogarticle, username=username, comment="＊タイトルを入れてください")
-        # else:
-        #     blogarticle.title = request.form.get('title')
-        #     blogarticle.body = request.form.get('body')
-        #     db.session.add(blogarticle)
-        #     db.session.commit()
-        #     return redirect(url_for('master'))
     else:
         return redirect(url_for('logout'))
 
+
 # 削除する場合(現状一発で削除されてしまう)
+# POSTのみ、GETで来たら強制ログアウトさせる
 @app.route('/delete', methods=['GET', 'POST'])
 @login_required
 def delete():
     if request.method == "POST":
         post_id = request.form.get("post_id")
-        blogarticle = BlogArticle.query.filter(BlogArticle.id == post_id).one()
+        blogarticle = BlogArticle.query.filter(BlogArticle.id == post_id).first()
         db.session.delete(blogarticle)
         db.session.commit()
         return redirect(url_for('master'))
@@ -211,6 +208,8 @@ def delete():
         return redirect(url_for('logout'))
 
 # ログアウト用
+# GETで来てもPOSTで来てもログアウトさせる
+# 持ってる情報、cookie,session共に消す
 @app.route('/logout', methods=['GET', 'POST'])
 @login_required
 def logout():
@@ -219,4 +218,5 @@ def logout():
     session.clear()
     res = make_response(redirect(url_for('blog')))
     res.delete_cookie('l_username')
+    res.delete_cookie('post_id')
     return res
